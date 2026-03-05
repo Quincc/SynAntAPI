45aa43c29f6d36306cbdc6ec09d8331f6dc470d5
Quincc <78716670+Quincc@users.noreply.github.com>
2026-03-05 21:14:02 +0500

    Add: SynonymsResponse AntonymsResponse StatsResponse WordBaseREsponse Change: WordResponse

--- app/schemas.py
+++ app/schemas.py
@@ -19,18 +19,38 @@ class WordItem(BaseModel):
     type: Literal["synonym", "antonym"] = Field(..., description="Тип: synonym или antonym")
 
 
-class WordResponse(BaseModel):
-    """Ответ сервера со списком синонимов и антонимов."""
-
-    original_word: str = Field(..., description="Исходное слово")
-    synonyms: list[WordItem] = Field(..., description="Список из 10 синонимов")
-    antonyms: list[WordItem] = Field(..., description="Список из 10 антонимов")
-
-
 class LLMWordList(BaseModel):
     """Структурированный ответ от LLM — список слов."""
 
     words: list[str] = Field(..., description="Список из 10 слов")
 
+
+class WordBaseResponse(BaseModel):
+    """Исходное слово"""
+
+    original_word: str = Field(..., description="Исходное слово")
+
+
+class SynonymsResponse(WordBaseResponse):
+    """Ответ сервера со списком синонимов"""
+
+    synonyms: list[WordItem] = Field(..., description="Список из 10 синонимов")
+
+
+class AntonymsResponse(WordBaseResponse):
+    """Ответ сервера со списком синонимов"""
+
+    antonyms: list[WordItem] = Field(..., description="Список из 10 антонимов")
+
+
+class WordResponse(SynonymsResponse, AntonymsResponse):
+    """Ответ сервера со списком синонимов и антонимов."""
+
+
 class HistoryItem(WordResponse):
     timestamp: datetime = Field(..., description="Время")
+
+
+class StatsResponse(BaseModel):
+    popular_word: str = Field(..., description="Самое популярное слово")
+    count_request: int = Field(..., description="Количество запросов")
